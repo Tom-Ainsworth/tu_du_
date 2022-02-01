e2b0c2520cfae5a04a5742516402deec67c8fd52
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-01 11:47:58 +0000

    Feat: Install art library and test

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 ''' Run a to do list via a CLI '''
 
 import json
+from art import *
+
+
+tprint('TU DU')
 
 with open('lists.json', 'r') as f:
     lists_json_file = json.load(f)
@@ -8,3 +12,4 @@ with open('lists.json', 'r') as f:
 ALL_LISTS = lists_json_file['Lists']
 LIST_TITLES = list(ALL_LISTS[0].keys())
 EXAMPLE_LIST_TITLE = ALL_LISTS[0].keys()
+
