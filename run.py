58231e9242ddb37e96cb3a76722479313b182877
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-01 13:52:39 +0000

    Feat: Add delay_print() and test. Add print statement to main()

--- run.py
+++ run.py
@@ -1,24 +1,51 @@
 ''' Run a to do list via a CLI '''
 
 import json
-from art import *
+import sys
+from time import sleep
+from art import tprint
+
+with open('lists.json', 'r') as f:
+    lists_json_file = json.load(f)
+
+# Constants
+ALL_LISTS = lists_json_file['Lists']
+LIST_TITLES = list(ALL_LISTS[0].keys())
+EXAMPLE_LIST_TITLE = ALL_LISTS[0].keys()
+
 
 def display_intro_text():
     '''
     Prints out introduction text to match the terminal rows/cols
     '''
 
+    sleep(0.5)
     print(f"{'=' * 80}")
     tprint('               Welcome')
+    sleep(1)
     tprint('                                    2')
+    sleep(1)
     tprint('                      Tu_Du_')
     print(f"{'=' * 80}")
 
-with open('lists.json', 'r') as f:
-    lists_json_file = json.load(f)
 
-ALL_LISTS = lists_json_file['Lists']
-LIST_TITLES = list(ALL_LISTS[0].keys())
-EXAMPLE_LIST_TITLE = ALL_LISTS[0].keys()
+def delay_print(string):
+    '''
+    Prints out a string 1 character at a time to aid readability for users
+    '''
+    
+    for character in string:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        sleep(0.05)
+    sleep(1)
 
-display_intro_text()
+def main():
+    '''
+    Main function to run on app openings
+    '''
+
+    display_intro_text()
+    delay_print('          The Productivity App that helps you manage your tasks')
+
+main()
