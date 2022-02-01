75541e0b1cff8fa6dc7a96fd87778dca67e05d72
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-01 12:55:45 +0000

    Feat: Add intro text function

--- run.py
+++ run.py
@@ -3,8 +3,16 @@
 import json
 from art import *
 
+def display_intro_text():
+    '''
+    Prints out introduction text to match the terminal rows/cols
+    '''
 
-tprint('TU DU')
+    print(f"{'=' * 80}")
+    tprint('               Welcome')
+    tprint('                                    2')
+    tprint('                      Tu_Du_')
+    print(f"{'=' * 80}")
 
 with open('lists.json', 'r') as f:
     lists_json_file = json.load(f)
@@ -13,3 +21,4 @@ ALL_LISTS = lists_json_file['Lists']
 LIST_TITLES = list(ALL_LISTS[0].keys())
 EXAMPLE_LIST_TITLE = ALL_LISTS[0].keys()
 
+display_intro_text()
