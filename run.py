d9f96d06cba3f91ddf1e96b1c2c438e96c013f4c
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-06 16:35:35 +0000

    Fix: give main_menu global scope, so I can update choices key within show_main_menu and other functions

--- run.py
+++ run.py
@@ -17,68 +17,85 @@ ALL_LISTS = lists_json_file['Lists']
 LIST_TITLES = [title for title in ALL_LISTS[0]]
 EXAMPLE_LIST_TITLE = LIST_TITLES[0]
 
-
-# def display_intro_text():
-#     '''
-#     Prints out introduction text to match the terminal rows/cols
-#     '''
-
-#     sleep(0.5)
-#     print(f"{'=' * 80}")
-#     tprint('               Welcome')
-#     sleep(1)
-#     tprint('                                    2')
-#     sleep(1)
-#     tprint('                      Tu_Du_')
-#     print(f"{'=' * 80}")
+# Prompt list format
+main_menu = [
+        {
+            "type": "list",
+            "name": "menu",
+            "message": "What would you like to do?"
+        }
+    ]
 
 
-# def delay_print(string):
-#     '''
-#     Prints out a string 1 character at a time to aid readability for users
-#     '''
+def show_list_titles_as_list():
+    '''
+    Takes all keys from the ALL_LISTS constant and puts them into a list
+    '''
 
-#     for character in string:
-#         sys.stdout.write(character)
-#         sys.stdout.flush()
-#         sleep(0.05)
-#     sleep(1)
+    choices = [title for title in ALL_LISTS[0]]
+    main_menu[0].update({"choices": choices})
 
 
-# def show_main_menu():
-#     '''
-#     Displays the user options featured at the start of the app
-#     '''
+def display_intro_text():
+    '''
+    Prints out introduction text to match the terminal rows/cols
+    '''
 
-#     main_menu = [
-#         {
-#             "type": "list",
-#             "name": "game",
-#             "message": "What would you like to do?",
-#             "choices": [
-#                 "Create a New List",
-#                 "See Instructions"
-#             ]
-#         }
-#     ]
-
-#     answer = prompt(main_menu).get("game")
-#     if answer == "\n\nWhat would you like to do?":
-#         create_new_list()
-#     else:
-#         show_instructions_page()
+    sleep(0.5)
+    print(f"{'=' * 80}")
+    tprint('               Welcome')
+    sleep(1)
+    tprint('                                    2')
+    sleep(1)
+    tprint('                      Tu_Du_')
+    print(f"{'=' * 80}")
 
 
-# def main():
-#     '''
-#     Main function to run on app openings
-#     '''
+def delay_print(string):
+    '''
+    Prints out a string 1 character at a time to aid readability for users
+    '''
 
-#     display_intro_text()
-#     delay_print(
-#         '\n          The productivity app that helps you manage your tasks\n'
-#         )
-#     show_main_menu()
+    for character in string:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        sleep(0.05)
+    sleep(1)
 
 
-# main()
+def show_main_menu():
+    '''
+    Displays the user options featured at the start of the app
+    '''
+
+    menu_choices = {"choices": [
+            "Create a New List", "Open Existing List", "Instructions Page"
+        ]
+    }
+    main_menu[0].update(menu_choices)
+
+    print(main_menu)
+
+    answer = prompt(main_menu).get("menu")
+    if answer == "Create a New List":
+        list_name = input('Choose list name: ')
+        create_new_list(list_name)
+    elif answer == "Open Existing List":
+        show_existing_lists()
+    else:
+        show_instructions_page()
+
+
+def main():
+    '''
+    Main function to run on app openings
+    '''
+
+    display_intro_text()
+    delay_print(
+        '\n          The productivity app that helps you manage your tasks\n'
+        )
+    show_main_menu()
+
+
+main()
