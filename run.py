705d5375d60261170b19e11057e33c95e32c538d
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-06 22:29:12 +0000

    Fix: Remove unnecessary constants

--- run.py
+++ run.py
@@ -9,93 +9,91 @@ from time import sleep
 from art import tprint
 from PyInquirer import prompt
 
-with open('lists.json', 'r', encoding="utf-8") as f:
-    lists_json_file = json.load(f)
-
-# Constants
-ALL_LISTS = lists_json_file['Lists']
-LIST_TITLES = [title for title in ALL_LISTS[0]]
-EXAMPLE_LIST_TITLE = LIST_TITLES[0]
-
 # Prompt list format
-main_menu = [
-        {
-            "type": "list",
-            "name": "menu",
-            "message": "What would you like to do?"
-        }
-    ]
+# questions = [
+#         {
+#             "type": "list",
+#             "name": "question",
+#             "message": "What would you like to do?"
+#         }
+#     ]
 
 
-def show_list_titles_as_list():
-    '''
-    Takes all keys from the ALL_LISTS constant and puts them into a list
-    '''
+# def show_list_titles_as_list():
+#     '''
+#     Takes all keys from the ALL_LISTS constant and puts them into a list
+#     '''
 
-    choices = [title for title in ALL_LISTS[0]]
-    main_menu[0].update({"choices": choices})
-
-
-def display_intro_text():
-    '''
-    Prints out introduction text to match the terminal rows/cols
-    '''
-
-    sleep(0.5)
-    print(f"{'=' * 80}")
-    tprint('               Welcome')
-    sleep(1)
-    tprint('                                    2')
-    sleep(1)
-    tprint('                      Tu_Du_')
-    print(f"{'=' * 80}")
+#     choices = [title for title in ALL_LISTS[0]]
+#     questions[0].update({"choices": choices})
 
 
-def delay_print(string):
-    '''
-    Prints out a string 1 character at a time to aid readability for users
-    '''
+# def display_intro_text():
+#     '''
+#     Prints out introduction text to match the terminal rows/cols
+#     '''
 
-    for character in string:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        sleep(0.05)
-    sleep(1)
-
-
-def show_main_menu():
-    '''
-    Displays the user options featured at the start of the app
-    '''
-
-    menu_choices = {"choices": [
-            "Create a New List", "Open Existing List", "Instructions Page"
-        ]
-    }
-    main_menu[0].update(menu_choices)
-
-    print(main_menu)
-
-    answer = prompt(main_menu).get("menu")
-    if answer == "Create a New List":
-        list_name = input('Choose list name: ')
-        create_new_list(list_name)
-    elif answer == "Open Existing List":
-        show_existing_lists()
-    else:
-        show_instructions_page()
+#     sleep(0.5)
+#     print(f"{'=' * 80}")
+#     tprint('               Welcome')
+#     sleep(1)
+#     tprint('                                    2')
+#     sleep(1)
+#     tprint('                      Tu_Du_')
+#     print(f"{'=' * 80}")
 
 
-def main():
-    '''
-    Main function to run on app openings
-    '''
+# def delay_print(string):
+#     '''
+#     Prints out a string 1 character at a time to aid readability for users
+#     '''
 
-    display_intro_text()
-    delay_print(
-        '\n          The productivity app that helps you manage your tasks\n'
-        )
-    show_main_menu()
+#     for character in string:
+#         sys.stdout.write(character)
+#         sys.stdout.flush()
+#         sleep(0.05)
+#     sleep(1)
 
 
-main()
+# def show_main_menu():
+#     '''
+#     Displays the user options featured at the start of the app
+#     '''
+
+#     menu_choices = {"choices": [
+#             "Create a New List", "Open Existing List", "Instructions Page"
+#         ]
+#     }
+#     questions[0].update(menu_choices)
+
+#     answer = prompt(questions).get("question")
+#     if answer == "Create a New List":
+#         list_name = input('Choose list name: ')
+#         create_new_list(list_name)
+#         delay_print(f'\nYour new list {list_name}has been created...')
+#         delay_print(f'\nNow opening {list_name}...')
+#     elif answer == "Open Existing List":
+#         show_existing_lists()
+#     else:
+#         show_instructions_page()
+
+
+# def main():
+#     '''
+#     Main function to run on app openings
+#     '''
+
+#     display_intro_text()
+#     delay_print(
+#         '\n          The productivity app that helps you manage your tasks\n'
+#         )
+#     show_main_menu()
+
+
+# main()
+
+with open('lists.json') as f:
+    data = json.load(f)
+    test_list = {"User List 2": ["a", "b", "c"]}
+    data["Lists"][-1].update(test_list)
+    print(data)
