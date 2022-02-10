f42d0701de6162b627f22ec820dcb0a94b7ac26f
Tom Ainsworth <tomainsworth94@gmail.com>
2022-02-10 16:02:58 +0000

    Fix: text ti display more neatly, and include tprint title

--- instructions.py
+++ instructions.py
@@ -4,6 +4,7 @@ Shows the instructions for Tu_Du
 
 from art import tprint
 from printing import delay_print, fast_delay_print
+from time import sleep
 
 
 def show_instructions_page():
@@ -11,8 +12,14 @@ def show_instructions_page():
     Displays the game instructions
     '''
 
+    fast_delay_print(f"{'=' * 80}\n\n")
+    tprint('     Instructions\n                         Page\n\n')
+    sleep(1)
+    fast_delay_print(f"{'=' * 80}\n\n")
+    sleep(1)
     with open('instructions.txt', 'r', encoding="utf-8") as file:
         lines = file.read()
         delay_print(lines)
+
 
 show_instructions_page()
